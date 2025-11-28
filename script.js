d8f64bb1eeca7592b7be33df839464d14f091b74
Brendan Hawk <brendanhawk03@aol.com>
2025-11-28 17:29:23 -0500

    Set up the HTML and started on the js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello bruh");
+//Connect HTML
+const notStartedCard = document.querySelector(".not-started-card");
+const inProgressCard = document.querySelector(".in-progress-card");
+const completedCard = document.querySelector(".completed-card");
+
+//Create modal to add new cards
+
+//Determine if modal is active or not
+
+//Create event listener for cards being dragged
